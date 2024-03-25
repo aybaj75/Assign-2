381531333eacc870fa48b04c3308b13c93898381
aybaj75 <144943838+aybaj75@users.noreply.github.com>
2024-03-24 23:38:10 -0400

    Add files via upload

--- Assignment 2.py
+++ Assignment 2.py
@@ -2,6 +2,7 @@
 #Ayaan Bajwa
 #100864399
 
+#Playsound Import to allow sound to implement
 from playsound import playsound
 
 def Sort(arr):
@@ -11,36 +12,48 @@ def Sort(arr):
         lH = arr[:mid]
         RH = arr[mid:]
         
-        Sort(lH)  # Recursively sort the left half
-        Sort(RH)  # Recursively sort the right half
+        Sort(lH)  # Recursiveley sort left half of the array and product id
+        Sort(RH)  # Recursively sort the right half of the array or product id
         
-        # Merge the sorted halves
+        # Merge sort begins 
+        
         i = j = k = 0
+        
+        #While loops for the merge sorts 
         
         while i < len(lH) and j < len(RH):
             if lH[i] < RH[j]:
                 arr[k] = lH[i]
                 i += 1
+                
             else:
                 arr[k] = RH[j]
                 j += 1
             k += 1
             playsound("beat.mp3.mp3")
+            #Playsound implemntation in while loop
+            
         # Check if any elements were left
         while i < len(lH):
             arr[k] = lH[i]
             i += 1
             k += 1
             playsound("beat.mp3.mp3")
+            #Playsound implemntation in while loop
+            
         while j < len(RH):
             arr[k] = RH[j]
             j += 1
             k += 1
             playsound("beat.mp3.mp3")
+            #Playsound implemntation in while loop
+            
+#Display function           
 def printlist(arr):
     print(' '.join(str(x) for x in arr))
 
-# Driver Program
+# Program Initiater 
+
 if __name__ == '__main__':
     arr = [0, 1, 3, 5, 7, 9, 2, 4, 6, 8]
     Sort(arr)
